5d5be622a11959a588749e20348ff5d7cfb5d0c1
jun7343 <zlsl7343@gmail.com>
2022-10-23 14:01:05 +0900

    ADD(config): QueryDSL, WebConfig 설정 class 추가
    - QueryDSL 설정 파일 추기
    - WebConfig default class 추가

---
+++ src/main/java/kr/jojoba/configs/WebConfig.java
@@ -0,0 +1,10 @@
+package kr.jojoba.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+}
