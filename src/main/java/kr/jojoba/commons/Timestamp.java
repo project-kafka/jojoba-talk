32d11af1f68511a0c6114bac4de6cb32da9c0193
jun7343 <zlsl7343@gmail.com>
2022-10-29 15:31:58 +0900

    FEAT(entity): Timestamp Entity 추가
    - 공통으로 사용되는 필드인 Timestamp 추가하여 중복코드 최소화(Auditing 기능 추가)
    
    Linked Issue:
    #2

---
+++ src/main/java/kr/jojoba/commons/Timestamp.java
@@ -0,0 +1,32 @@
+package kr.jojoba.commons;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@EqualsAndHashCode(exclude = "updatedAt")
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Timestamp {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    private LocalDateTime updatedAt;
+}
