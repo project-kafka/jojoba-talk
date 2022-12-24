2726dba3bed53dc6ee202c1307d3803df0fb9eab
jinseonghwang <eddy5360@naver.com>
2022-12-24 17:40:06 +0900

    FEAT(user): 유저 가입, 조회 API 구현
    resolved #6

--- src/main/java/kr/jojoba/user/application/UserService.java
+++ src/main/java/kr/jojoba/user/application/UserService.java
@@ -1,5 +1,10 @@
 package kr.jojoba.user.application;
 
+import java.util.UUID;
+import kr.jojoba.errors.NotFoundException;
+import kr.jojoba.user.domain.User;
+import kr.jojoba.user.domain.dto.UserCreateRequestDto;
+import kr.jojoba.user.domain.dto.UserResponseDto;
 import kr.jojoba.user.infrastructure.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +14,21 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public UserResponseDto createUser(UserCreateRequestDto requestDto) {
+        final User user = User.builder()
+                              .email(requestDto.getEmail())
+                              .password(requestDto.getPassword())
+                              .build();
+        userRepository.save(user);
+        return UserResponseDto.of(user);
+    }
+
+    public UserResponseDto getUserById(String id) {
+        final User user = userRepository.findById(UUID.fromString(id))
+                                        .orElseThrow(() -> new NotFoundException(
+                                            String.format("Cannot found user entity [id = %s]", id)
+                                        ));
+        return UserResponseDto.of(user);
+    }
 }
---
+++ src/main/java/kr/jojoba/user/domain/dto/UserCreateRequestDto.java
@@ -0,0 +1,16 @@
+package kr.jojoba.user.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCreateRequestDto {
+
+    private String email;
+    private String password;
+}
